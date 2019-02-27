337ce29c0032f1302212169e3a8bd6e7c2b42872
naoki <n.globe.us@gmail.com>
2019-02-27 17:16:04 +0900

    Add action hook, and callback. This is temporary commit. Haven't checked.

--- connect-to-alis.php
+++ connect-to-alis.php
@@ -30,3 +30,45 @@ function cta_alis_api_key( $args ) {
 }
 
 //If post published -> get the post id -> get title,content,thumbnail_url,overview?.
+add_action( 'transition_post_status', 'post_unpublished', 10, 3 );
+function post_unpublished( $new_status, $old_status, $post ) {
+
+	if ( $old_status == 'new'  &&  $new_status == 'publish' || $old_status == 'pending' && $new_status == 'publish' || $old_status == 'draft' && $new_status == 'publish'|| $old_status == 'future' && $new_status == 'publish' || $old_status == 'auto-draft' && $new_status == 'publish' ) {
+
+		$token    = '';
+		$base_url = 'https://alis.to/api/me/articles/drafts';
+
+		$data = [
+			'title'      => 'example',
+			'body' => 'テスト',
+			'eye_catch_url'   => 'https://alis.to/d/api/info_icon/naogify/icon/1c508b18-8aa3-489e-91cc-9d707d8cc443.jpeg',      // テストで作る時は限定公開で
+			'overview'   => 'sample test',
+		];
+
+		$header = [
+			'accept: application/json',
+			'Authorization: ' . $token,
+			'Content-Type: application/json',
+		];
+
+		$curl = curl_init();
+
+		curl_setopt( $curl, CURLOPT_URL, $base_url . '/api/v2/items' );
+		curl_setopt( $curl, CURLOPT_CUSTOMREQUEST, 'POST' ); // post
+		curl_setopt( $curl, CURLOPT_POSTFIELDS, json_encode( $data ) ); // jsonデータを送信
+		curl_setopt( $curl, CURLOPT_HTTPHEADER, $header ); // リクエストにヘッダーを含める
+		curl_setopt( $curl, CURLOPT_SSL_VERIFYPEER, false );
+		curl_setopt( $curl, CURLOPT_RETURNTRANSFER, true );
+		curl_setopt( $curl, CURLOPT_HEADER, true );
+
+		$response = curl_exec( $curl );
+
+
+//		$header_size = curl_getinfo( $curl, CURLINFO_HEADER_SIZE );
+//		$header      = substr( $response, 0, $header_size );
+//		$body        = substr( $response, $header_size );
+//		$result      = json_decode( $body, true );
+
+		curl_close( $curl );
+	}
+}
