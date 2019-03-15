2f21887c8ac4f2c6183a53b68d029c8869141ca5
naoki <n.globe.us@gmail.com>
2019-03-16 03:20:28 +0900

    Prepare default.jpg

--- connect-to-alis.php
+++ connect-to-alis.php
@@ -12,7 +12,7 @@
  * @package         Connect_To_Alis
  */
 
-
+add_theme_support('post-thumbnails');
 //
 add_action( 'admin_init', 'add_general_custom_sections' );
 function add_general_custom_sections() {
@@ -38,19 +38,26 @@ function post_unpublished( $new_status, $old_status, $post ) {
 
 	if ( $old_status == 'new'  &&  $new_status == 'publish' || $old_status == 'pending' && $new_status == 'publish' || $old_status == 'draft' && $new_status == 'publish'|| $old_status == 'future' && $new_status == 'publish' || $old_status == 'auto-draft' && $new_status == 'publish' ) {
 
-		$token    = '';
-		$base_url = 'https://alis.to/api/me/articles/drafts';
+		$token     = '';
+		$base_url  = 'https://alis.to/api/me/articles/drafts';
+		$title     = esc_html( $post->post_title );
+		$thumbnail = get_the_post_thumbnail_url( $post, 'full' );
+
+//		If there is No thumbnail, it will use default one.
+		if ( ! isset( $thumbnail ) ) {
+			$thumbnail = esc_url( content_url() ) . '/plugins/connect-to-alis/assets/thumbnail.jpg';
+		}
 
 		$data = [
-			'title'         => 'ポスト from WordPress2',
-			'body'          => 'テスト',
-			'eye_catch_url' => 'https://alis.to/d/api/info_icon/naogify/icon/1c508b18-8aa3-489e-91cc-9d707d8cc443.jpeg',      // テストで作る時は限定公開で
-			'overview'      => 'sample test',
+			'title'         => $title,
+			'body'          => wp_strip_all_tags( $post->post_content ),
+			'eye_catch_url' => esc_url( $thumbnail ),
+			'overview'      => $title,
 		];
 
 		$header = [
 			'accept: application/json',
-			'Authorization: ' . $token,
+			'Authorization: ' . esc_html( $token ),
 			'Content-Type: application/json',
 		];
 
@@ -70,43 +77,8 @@ function post_unpublished( $new_status, $old_status, $post ) {
 		$body        = substr( $response, $header_size );
 		$result      = json_decode( $body, true );
 
-		update_option( 'alis-dev-value', $result );
 		curl_close( $curl );
 
-
-		//下書き一覧を取得
-//		$token    = '';
-//		$base_url = 'https://alis.to/api/me/articles/drafts?limit=2';
-//
-//		$data = [
-//			'title'         => 'ポスト from WordPress',
-//			'body'          => 'テスト',
-//			'eye_catch_url' => 'https://alis.to/d/api/info_icon/naogify/icon/1c508b18-8aa3-489e-91cc-9d707d8cc443.jpeg',      // テストで作る時は限定公開で
-//			'overview'      => 'sample test',
-//		];
-//
-//		$header = [
-//			'accept: application/json',
-//			'Authorization: ' . $token,
-//		];
-//
-//		$curl = curl_init();
-//
-//		curl_setopt( $curl, CURLOPT_URL, $base_url );
-//		curl_setopt( $curl, CURLOPT_CUSTOMREQUEST, 'GET' ); // post
-//		curl_setopt( $curl, CURLOPT_HTTPHEADER, $header ); // リクエストにヘッダーを含める
-//		curl_setopt( $curl, CURLOPT_SSL_VERIFYPEER, false );
-//		curl_setopt( $curl, CURLOPT_RETURNTRANSFER, true );
-//		curl_setopt( $curl, CURLOPT_HEADER, true );
-//
-//		$response    = curl_exec( $curl );
-//		$header_size = curl_getinfo( $curl, CURLINFO_HEADER_SIZE );
-//		$header      = substr( $response, 0, $header_size );
-//		$body        = substr( $response, $header_size );
-//		$result      = json_decode( $body, true );
-//
-//		update_option( 'alis-dev-value', $response );
-//
-//		curl_close( $curl );
+		update_option( 'alis-dev-value', $result );
 	}
 }
