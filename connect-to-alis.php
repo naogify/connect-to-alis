654ee4f52cdfb501dc4029fcbbd1a57ff92d0ee2
naoki <n.globe.us@gmail.com>
2019-03-17 08:44:19 +0900

    aws-sdkを設定

--- connect-to-alis.php
+++ connect-to-alis.php
@@ -12,8 +12,8 @@
  * @package         Connect_To_Alis
  */
 
-add_theme_support('post-thumbnails');
-//
+add_theme_support( 'post-thumbnails' );
+
 add_action( 'admin_init', 'add_general_custom_sections' );
 function add_general_custom_sections() {
 	// register_setting( 'general', 'キー' )で値を保存
@@ -33,18 +33,20 @@ function cta_alis_api_key( $args ) {
 }
 
 //If post published -> get the post id -> get title,content,thumbnail_url,overview?.
-add_action( 'transition_post_status', 'post_unpublished', 10, 3 );
-function post_unpublished( $new_status, $old_status, $post ) {
+add_action( 'transition_post_status', 'cta_alis_post_published', 10, 3 );
+function cta_alis_post_published( $new_status, $old_status, $post ) {
+
+	$post_thumbnail_id = get_post_thumbnail_id( $post );
 
 	if ( $old_status == 'new'  &&  $new_status == 'publish' || $old_status == 'pending' && $new_status == 'publish' || $old_status == 'draft' && $new_status == 'publish'|| $old_status == 'future' && $new_status == 'publish' || $old_status == 'auto-draft' && $new_status == 'publish' ) {
 
-		$token     = '';
-		$base_url  = 'https://alis.to/api/me/articles/drafts';
-		$title     = esc_html( $post->post_title );
-		$thumbnail = get_the_post_thumbnail_url( $post, 'full' );
+		$token    = 'eyJraWQiOiJEdHdzaHg2ZVkxNkpkdlc4MnpMU3o0ajNXOVpVRHhpY3Vvb0pST1NWanRBPSIsImFsZyI6IlJTMjU2In0.eyJzdWIiOiI1M2MwYjExYS0zNzIwLTRiMDItODJmNS1mNGEwMzczM2NhYTMiLCJlbWFpbF92ZXJpZmllZCI6dHJ1ZSwiaXNzIjoiaHR0cHM6XC9cL2NvZ25pdG8taWRwLmFwLW5vcnRoZWFzdC0xLmFtYXpvbmF3cy5jb21cL2FwLW5vcnRoZWFzdC0xX0hOVDBmVWo0SiIsInBob25lX251bWJlcl92ZXJpZmllZCI6dHJ1ZSwiY29nbml0bzp1c2VybmFtZSI6Im5hb2dpZnkiLCJhdWQiOiIyZ3JpNWl1dWt2ZTMwMmk0Z2hjbGg2cDVyZyIsImV2ZW50X2lkIjoiZDQzYmJlNDMtNDczMy0xMWU5LWI0ZGQtZmY4ZjY1NTdlMDJkIiwidG9rZW5fdXNlIjoiaWQiLCJhdXRoX3RpbWUiOjE1NTI2NjIzNTQsInBob25lX251bWJlciI6Iis4MTgwOTQ3MjI3OTYiLCJjdXN0b206cHJpdmF0ZV9ldGhfYWRkcmVzcyI6IjB4MDM5YmQ0ZjhjNmNlNzAwMTU0ZGE3NzFjNzliYTgyYzM4Mzg3ZTEzYSIsImV4cCI6MTU1MjY3OTUyNiwiaWF0IjoxNTUyNjc1OTI2LCJlbWFpbCI6Im4uZ2xvYmUudXMrdHdpaXRlckBnbWFpbC5jb20ifQ.cAZHLjUSnujnhJfWaMvN5CK7NdvUfCZesD1Og93gvJ87_4OT-bEjc73Y80J_JgTz5bQ5z8ZPVPJ-pIyOSgeqn1CZESjq5zfyn4CW5JnOMPRz6mQ4guaT7_ShSs85LQBeh6CxdONAjZ5lTMyHw5wsRlBEnoDqQdEmeprjUHeYe6KVR743DCZWnerxgVmypqD2P_OG3ViCK-BpGuTY-zXoDW6Ot2eDkIfQ4ijp-m0izoVkUD-W7COepn6Jak8sOsMGV8Pm7rP0_WhnF-BqUqCbUpXw505hTJPBparvjskBlbs7aGxm0joNqKamgrTD4IO6GvdbgyoCSEVWkBjQNchMKw';
+		$base_url = 'https://alis.to/api/me/articles/drafts';
+		$title    = esc_html( $post->post_title );
 
-//		If there is No thumbnail, it will use default one.
-		if ( ! isset( $thumbnail ) ) {
+		if ( has_post_thumbnail() ) {
+			$thumbnail = get_the_post_thumbnail_url( (int) $post->ID, 'full' );
+		} else {
 			$thumbnail = esc_url( content_url() ) . '/plugins/connect-to-alis/assets/thumbnail.jpg';
 		}
 
@@ -79,6 +81,6 @@ function post_unpublished( $new_status, $old_status, $post ) {
 
 		curl_close( $curl );
 
-		update_option( 'alis-dev-value', $result );
+		update_option( 'alis-dev-value', $post_thumbnail_id );
 	}
 }
