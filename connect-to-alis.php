ad819e558ea9b5e4e0f6017b064cd3e5aac8f9de
naoki <n.globe.us@gmail.com>
2019-03-23 09:16:04 +0900

    Fix Some

--- connect-to-alis.php
+++ connect-to-alis.php
@@ -3,8 +3,8 @@
  * Plugin Name:     Connect To Alis
  * Plugin URI:      PLUGIN SITE HERE
  * Description:     PLUGIN DESCRIPTION HERE
- * Author:          YOUR NAME HERE
- * Author URI:      YOUR SITE HERE
+ * Author:          Naoki Ohashi
+ * Author URI:      https://naoki-is.me/
  * Text Domain:     connect-to-alis
  * Domain Path:     /languages
  * Version:         0.1.0
@@ -16,23 +16,31 @@ add_theme_support( 'post-thumbnails' );
 
 add_action( 'admin_init', 'add_general_custom_sections' );
 function add_general_custom_sections() {
-	// register_setting( 'general', 'キー' )で値を保存
-	register_setting( 'general', 'cta_alis_api_key' );
-	// add_settings_field( 'キー', 'ラベル', 'コールバック関数', 'general' )で項目を追加
-	add_settings_field( 'cta_alis_api_key', 'Alis API key', 'cta_alis_api_key', 'general' );
 
-	var_dump( get_option( 'alis-dev-value' ) );
+	register_setting( 'general', 'cta_alis_username' );
+	add_settings_field( 'cta_alis_username', 'Alis Username', 'cta_alis_username', 'general' );
+
+	register_setting( 'general', 'cta_alis_password' );
+	add_settings_field( 'cta_alis_password', 'Alis Password', 'cta_alis_password', 'general' );
 }
 
-function cta_alis_api_key( $args ) {
-	$alis_api_key = get_option( 'cta_alis_api_key' );
+function cta_alis_username( $args ) {
+	$alis_username = get_option( 'cta_alis_username' );
 	?>
-    <input type="text" name="cta_alis_api_key" id="cta_alis_api_key" size="30"
-           value="<?php echo esc_attr( $alis_api_key ); ?>"/>
+    <input type="password" name="cta_alis_username" id="cta_alis_username" size="30"
+           value="<?php echo esc_attr( $alis_username ); ?>"/>
 	<?php
 }
 
-//If post published -> get the post id -> get title,content,thumbnail_url,overview?.
+function cta_alis_password( $args ) {
+	$alis_password = get_option( 'cta_alis_password' );
+	?>
+    <input type="password" name="cta_alis_password" id="cta_alis_password" size="30"
+           value="<?php echo esc_attr( $alis_password ); ?>"/>
+	<?php
+}
+
+
 add_action( 'transition_post_status', 'cta_alis_post_published', 10, 3 );
 function cta_alis_post_published( $new_status, $old_status, $post ) {
 
@@ -40,14 +48,15 @@ function cta_alis_post_published( $new_status, $old_status, $post ) {
 
 	if ( $old_status == 'new'  &&  $new_status == 'publish' || $old_status == 'pending' && $new_status == 'publish' || $old_status == 'draft' && $new_status == 'publish'|| $old_status == 'future' && $new_status == 'publish' || $old_status == 'auto-draft' && $new_status == 'publish' ) {
 
-		$token    = 'eyJraWQiOiJEdHdzaHg2ZVkxNkpkdlc4MnpMU3o0ajNXOVpVRHhpY3Vvb0pST1NWanRBPSIsImFsZyI6IlJTMjU2In0.eyJzdWIiOiI1M2MwYjExYS0zNzIwLTRiMDItODJmNS1mNGEwMzczM2NhYTMiLCJlbWFpbF92ZXJpZmllZCI6dHJ1ZSwiaXNzIjoiaHR0cHM6XC9cL2NvZ25pdG8taWRwLmFwLW5vcnRoZWFzdC0xLmFtYXpvbmF3cy5jb21cL2FwLW5vcnRoZWFzdC0xX0hOVDBmVWo0SiIsInBob25lX251bWJlcl92ZXJpZmllZCI6dHJ1ZSwiY29nbml0bzp1c2VybmFtZSI6Im5hb2dpZnkiLCJhdWQiOiIyZ3JpNWl1dWt2ZTMwMmk0Z2hjbGg2cDVyZyIsImV2ZW50X2lkIjoiZDQzYmJlNDMtNDczMy0xMWU5LWI0ZGQtZmY4ZjY1NTdlMDJkIiwidG9rZW5fdXNlIjoiaWQiLCJhdXRoX3RpbWUiOjE1NTI2NjIzNTQsInBob25lX251bWJlciI6Iis4MTgwOTQ3MjI3OTYiLCJjdXN0b206cHJpdmF0ZV9ldGhfYWRkcmVzcyI6IjB4MDM5YmQ0ZjhjNmNlNzAwMTU0ZGE3NzFjNzliYTgyYzM4Mzg3ZTEzYSIsImV4cCI6MTU1MjY3OTUyNiwiaWF0IjoxNTUyNjc1OTI2LCJlbWFpbCI6Im4uZ2xvYmUudXMrdHdpaXRlckBnbWFpbC5jb20ifQ.cAZHLjUSnujnhJfWaMvN5CK7NdvUfCZesD1Og93gvJ87_4OT-bEjc73Y80J_JgTz5bQ5z8ZPVPJ-pIyOSgeqn1CZESjq5zfyn4CW5JnOMPRz6mQ4guaT7_ShSs85LQBeh6CxdONAjZ5lTMyHw5wsRlBEnoDqQdEmeprjUHeYe6KVR743DCZWnerxgVmypqD2P_OG3ViCK-BpGuTY-zXoDW6Ot2eDkIfQ4ijp-m0izoVkUD-W7COepn6Jak8sOsMGV8Pm7rP0_WhnF-BqUqCbUpXw505hTJPBparvjskBlbs7aGxm0joNqKamgrTD4IO6GvdbgyoCSEVWkBjQNchMKw';
+
+		$token    = '';
 		$base_url = 'https://alis.to/api/me/articles/drafts';
 		$title    = esc_html( $post->post_title );
 
 		if ( has_post_thumbnail() ) {
 			$thumbnail = get_the_post_thumbnail_url( (int) $post->ID, 'full' );
 		} else {
-			$thumbnail = esc_url( content_url() ) . '/plugins/connect-to-alis/assets/thumbnail.jpg';
+			$thumbnail = esc_url( content_url() ) . '/plugins/connect-to-alis/assets/thumbnail02.jpg';
 		}
 
 		$data = [
@@ -66,9 +75,9 @@ function cta_alis_post_published( $new_status, $old_status, $post ) {
 		$curl = curl_init();
 
 		curl_setopt( $curl, CURLOPT_URL, $base_url );
-		curl_setopt( $curl, CURLOPT_CUSTOMREQUEST, 'POST' ); // post
-		curl_setopt( $curl, CURLOPT_POSTFIELDS, json_encode( $data ) ); // jsonデータを送信
-		curl_setopt( $curl, CURLOPT_HTTPHEADER, $header ); // リクエストにヘッダーを含める
+		curl_setopt( $curl, CURLOPT_CUSTOMREQUEST, 'POST' );
+		curl_setopt( $curl, CURLOPT_POSTFIELDS, json_encode( $data ) );
+		curl_setopt( $curl, CURLOPT_HTTPHEADER, $header );
 		curl_setopt( $curl, CURLOPT_SSL_VERIFYPEER, false );
 		curl_setopt( $curl, CURLOPT_RETURNTRANSFER, true );
 		curl_setopt( $curl, CURLOPT_HEADER, true );
@@ -81,6 +90,20 @@ function cta_alis_post_published( $new_status, $old_status, $post ) {
 
 		curl_close( $curl );
 
-		update_option( 'alis-dev-value', $post_thumbnail_id );
+//		update_option( 'alis-dev-value', $post_thumbnail_id );
 	}
 }
+
+add_action( 'admin_enqueue_scripts', 'cta_alis_load_api_scripts' );
+function cta_alis_load_api_scripts( $hook ) {
+
+    var_dump(plugin_dir_url(__FILE__));
+
+	if ( 'edit.php' == $hook || 'post-new.php' == $hook ) {
+
+		if ( current_user_can( 'administrator' ) ) {
+			wp_enqueue_script( 'alis_api_scripts', plugin_dir_url(__FILE__) . '/dist/my-app.js', array(), '1.0.0', true );
+		}
+	}
+}
+
--- src/entry.js
+++ src/entry.js
@@ -3,17 +3,16 @@ var AmazonCognitoIdentity = require('amazon-cognito-identity-js');
 var CognitoUserPool = AmazonCognitoIdentity.CognitoUserPool;
 
 var authenticationData = {
-    Username : 'username',
-    Password : 'password',
+    Username : '',
+    Password : '',
 };
 var authenticationDetails = new AmazonCognitoIdentity.AuthenticationDetails(authenticationData);
-var poolData = {
-    UserPoolId : '...', // Your user pool id here
-    ClientId : '...' // Your client id here
+var poolData = { UserPoolId : 'ap-northeast-1_HNT0fUj4J',
+    ClientId : '2gri5iuukve302i4ghclh6p5rg'
 };
 var userPool = new AmazonCognitoIdentity.CognitoUserPool(poolData);
 var userData = {
-    Username : 'username',
+    Username : '',
     Pool : userPool
 };
 var cognitoUser = new AmazonCognitoIdentity.CognitoUser(userData);
@@ -21,31 +20,14 @@ cognitoUser.authenticateUser(authenticationDetails, {
     onSuccess: function (result) {
         var accessToken = result.getAccessToken().getJwtToken();
 
-        //POTENTIAL: Region needs to be set if not already set previously elsewhere.
-        AWS.config.region = '<region>';
+        /* Use the idToken for Logins Map when Federating User Pools with identity pools or when passing through an Authorization Header to an API Gateway Authorizer */
+        var idToken = result.idToken.jwtToken;
 
-        AWS.config.credentials = new AWS.CognitoIdentityCredentials({
-            IdentityPoolId : '...', // your identity pool id here
-            Logins : {
-                // Change the key below according to the specific region your user pool is in.
-                'cognito-idp.<region>.amazonaws.com/<YOUR_USER_POOL_ID>' : result.getIdToken().getJwtToken()
-            }
-        });
-
-        //refreshes credentials using AWS.CognitoIdentity.getCredentialsForIdentity()
-        AWS.config.credentials.refresh((error) => {
-            if (error) {
-                console.error(error);
-            } else {
-                // Instantiate aws sdk service objects now that the credentials have been updated.
-                // example: var s3 = new AWS.S3();
-                console.log('Successfully logged!');
-            }
-        });
     },
 
     onFailure: function(err) {
-        alert(err.message || JSON.stringify(err));
+        console.log(err);
+        alert(err);
     },
 
 });
